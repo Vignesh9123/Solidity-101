d05da9667c2a1a72cd5e89b973d1d268a952bad3
Vignesh D <vignesh.d9123@gmail.com>
2025-05-21 20:01:16 +0530

    Variables

---
+++ Variables/Sum.sol
@@ -0,0 +1,30 @@
+pragma solidity 0.8.26
+contract Sum{
+    // Unsigned 8-bit integer
+    uint8 num;
+
+    // Unsigned 16-bit integer
+    uint16 num2;
+
+    // Unsigned 256-bit integer
+    uint256 num3;
+
+    // Signed 8-bit integer
+    int8 num4;
+
+    // Signed 16-bit integer
+    int16 num5;
+
+    // Signed 256-bit integer (DEFAULT)
+    int256 num6;
+
+    // Boolean
+    bool isActive;
+
+    // String
+    string name = "Vignesh";
+
+    // Address
+    address owner = 0x4C590bCBeB15093f2C5bdB4F460e9F9f90a4046C;    
+
+}
